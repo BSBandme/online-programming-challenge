31b846ea43ddd5ea5946c52d05273df1df46528b
BSBandme <chongyilun250@sina.com>
2016-01-12 16:36:29 -0500

    add

---
+++ Topcoder/SRM677_DIV1/500.cpp
@@ -0,0 +1,223 @@
+/*
+ * temp.cpp
+ *
+ *  Created on: 2012-7-18
+ *      Author: BSBandme
+ */
+//#pragma comment(linker, "/STACK:1024000000,1024000000")
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <cstdio>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cassert>
+#include <list>
+#include <iomanip>
+#include <math.h>
+#include <deque>
+#include <utility>
+#include <map>
+#include <set>
+#include <bitset>
+#include <numeric>
+#include <climits>
+#include <cctype>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <functional>
+#include <sstream>
+#include <tr1/unordered_set>
+#include <tr1/unordered_map>
+
+using namespace std;
+using namespace tr1;
+
+#define mpr make_pair
+typedef unsigned int ui;
+typedef unsigned long long ull;
+typedef long long ll;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <double, double> pdd;
+typedef vector <int> vi;
+typedef vector <ll> vll;
+typedef vector <double> vd;
+typedef vector <string> vs;
+typedef map <string, int> mpsi;
+typedef map <double, int> mpdi;
+typedef map <int, int> mpii;
+
+const double pi = acos(0.0) * 2.0;
+const double eps = 1e-12;
+const int step[8][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+
+template <class T> inline T abs1(T a) {return a < 0 ? -a : a;}
+
+template <class T> inline T max1(T a, T b) { return a > b ? a : b; }
+template <class T> inline T max1(T a, T b, T c) { return max1(max1(a, b), c); }
+template <class T> inline T max1(T a, T b, T c, T d) { return max1(max1(a, b, c), d); }
+template <class T> inline T max1(T a, T b, T c, T d, T e) { return max1(max1(a, b, c, d), e); }
+template <class T> inline T min1(T a, T b) { return a < b ? a : b; }
+template <class T> inline T min1(T a, T b, T c) { return min1(min1(a, b), c); }
+template <class T> inline T min1(T a, T b, T c, T d) { return min1(min1(a, b, c), d); }
+template <class T> inline T min1(T a, T b, T c, T d, T e) { return min1(min1(a, b, c, d), e); }
+
+inline int jud(double a, double b){
+	if(abs(a) < eps && abs(b) < eps) return 0;
+	else if(abs1(a - b) / abs1(a) < eps) return 0;
+	if(a < b) return -1;
+	return 1;
+}
+template <typename t> inline int jud(t a, t b){
+	if(a < b) return -1;
+	if(a == b) return 0;
+	return 1;
+}
+
+// f_lb == 1代表返回相同的一串的左边界，f_small == 1代表返回如果没有寻找的值返回小的数
+template <typename it, typename t1>
+inline int find(t1 val, it a, int na, bool f_small = 1, bool f_lb = 1){
+	int be = 0, en = na - 1;
+	if(*a <= *(a + na - 1)){
+		if(f_lb == 0) while(be < en){
+			int mid = (be + en + 1) / 2;
+			if(jud(*(a + mid), val) != 1) be = mid;
+			else en = mid - 1;
+		}else while(be < en){
+			int mid = (be + en) / 2;
+			if(jud(*(a + mid), val) != -1) en = mid;
+			else be = mid + 1;
+		}
+		if(f_small && jud(*(a + be), val) == 1) be--;
+		if(!f_small && jud(*(a + be), val) == -1) be++;
+	} else {
+		if(f_lb) while(be < en){
+			int mid = (be + en + 1) / 2;
+			if(jud(*(a + mid), val) != -1) be = mid;
+			else en = mid - 1;
+		}else while(be < en){
+			int mid = (be + en) / 2;
+			if(jud(*(a + mid), val) != 1) en = mid;
+			else be = mid + 1;
+		}
+		if(!f_small && jud(*(a + be), val) == -1) be--;
+		if(f_small && jud(*(a + be), val) == 1) be++;
+	}
+	return be;
+}
+
+template <class T> inline T lowb(T num) {return num & (-num); }
+inline int bitnum(ui nValue) { return __builtin_popcount(nValue); }
+inline int bitnum(int nValue) { return __builtin_popcount(nValue); }
+inline int bitnum(ull nValue) { return __builtin_popcount(nValue) + __builtin_popcount(nValue >> 32); }
+inline int bitnum(ll nValue) { return __builtin_popcount(nValue) + __builtin_popcount(nValue >> 32); }
+inline int bitmaxl(ui a) { if(a == 0) return 0; return 32 - __builtin_clz(a); }
+inline int bitmaxl(int a) { if(a == 0) return 0; return 32 - __builtin_clz(a); }
+inline int bitmaxl(ull a) { int temp = a >> 32; if(temp) return 32 - __builtin_clz(temp) + 32; return bitmaxl(int(a)); }
+inline int bitmaxl(ll a) { int temp = a >> 32; if(temp) return 32 - __builtin_clz(temp) + 32; return bitmaxl(int(a)); }
+
+long long pow(long long n, long long m, long long mod = 0){
+	if(m < 0) return 0;
+	long long ans = 1;
+	long long k = n;
+	while(m){
+		if(m & 1) {
+			ans *= k;
+			if(mod) ans %= mod;
+		}
+		k *= k;
+		if(mod) k %= mod;
+		m >>= 1;
+	}
+	return ans;
+}
+template <class t>
+void output1(t arr) {
+	for(int i = 0; i < (int)arr.size(); i++)
+		cerr << arr[i] << ' ';
+	cerr << endl;
+}
+template <class t>
+void output2(t arr) {
+	for(int i = 0; i < (int)arr.size(); i++)
+		output1(arr[i]);
+}
+#define  MOD 1000000007
+template <class t1, class t2>
+inline void add(t1 &a, t2 b, int mod = -1) {
+	if(mod == -1) mod = MOD;
+	a += b;
+	while(a >= mod) a -= mod;
+	while(a < 0) a += mod;
+}
+
+#define debug
+//.........................mi.......feng......xian.......xia.......jin.......zhi.......hack...............................................
+
+const int maxn = 55;
+double dp[maxn][maxn * 2][maxn * 2];
+struct edge {
+	int to, nxt;
+} e[maxn * 2];
+int head[maxn], le;
+int sz[maxn];
+double temp[maxn * 2][maxn * 2];
+
+void addedge(int a, int b) {
+	e[le].to = b;
+	e[le].nxt = head[a];
+	head[a] = le++;
+}
+
+void dfs(int no, int fa = -1) {
+	memset(dp[no], 0, sizeof(dp[no]));
+	dp[no][0][0] = 1;
+	sz[no] = 1;
+	for(int i = head[no]; i != -1; i = e[i].nxt) if(e[i].to != fa){
+		dfs(e[i].to, no	);
+		memset(temp, 0, sizeof(temp));
+		for(int rl = 0; rl <= sz[e[i].to] * 2; rl++)
+			for(int rd = 0; rd <= sz[e[i].to] * 2; rd++)
+				for(int l = 0; l <= sz[no] * 2; l++)
+					for(int d = 0; d <= sz[no] * 2; d++) {
+						temp[max(l, rl + 1)][max1(d, rd, rl + 1 + l)] += dp[no][l][d] * dp[e[i].to][rl][rd] * 0.5;
+						temp[max(l, rl + 2)][max1(d, rd, rl + 2 + l)] += dp[no][l][d] * dp[e[i].to][rl][rd] * 0.5;
+					}
+		sz[no] += sz[e[i].to];
+		memcpy(dp[no], temp, sizeof(temp));
+	}
+}
+
+struct DiameterOfRandomTree{
+	double getExpectation(vector <int> a, vector <int> b){
+		le = 0;
+		memset(head, -1, sizeof(head));
+		memset(dp, 0, sizeof(dp));
+		for(int i = 0; i < a.size(); i++) {
+			addedge(a[i], b[i]);
+			addedge(b[i], a[i]);
+		}
+		dfs(0);
+		double ans = 0;
+		for(int i = 0; i < maxn * 2; i++)
+			for(int j = 0; j < maxn * 2; j++) {
+				ans += dp[0][i][j] * j;
+			}
+
+		return ans;
+	}
+	
+
+
+};
+
+
+
+
+// Powered by FileEdit
+// Powered by TZTester 1.01 [25-Feb-2003]
+// Powered by CodeProcessor
